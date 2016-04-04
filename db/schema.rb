b2f524fa589c96fae618f4138d644829d7c2cf30
COLIN <reckocp@gmail.com>
2016-04-04 11:05:35 -0500

    Switched to Heroku-friendly format

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160331190129) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "links", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "title",      null: false
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20160331190129) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "links", ["user_id"], name: "index_links_on_user_id"
+  add_index "links", ["user_id"], name: "index_links_on_user_id", using: :btree
 
   create_table "upvotes", force: :cascade do |t|
     t.integer  "link_id"
@@ -29,7 +32,7 @@ ActiveRecord::Schema.define(version: 20160331190129) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "upvotes", ["link_id"], name: "index_upvotes_on_link_id"
+  add_index "upvotes", ["link_id"], name: "index_upvotes_on_link_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name",       null: false
@@ -38,4 +41,6 @@ ActiveRecord::Schema.define(version: 20160331190129) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "links", "users"
+  add_foreign_key "upvotes", "links"
 end
